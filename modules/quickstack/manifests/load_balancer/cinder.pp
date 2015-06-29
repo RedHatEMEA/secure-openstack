702ff3b527364ff23a6875343676e086a9a8c62f
Alessandro Vozza <alessandro@ams0.org>
2015-06-29 23:18:23 +0200

    added if/else

--- modules/quickstack/manifests/load_balancer/cinder.pp
+++ modules/quickstack/manifests/load_balancer/cinder.pp
@@ -7,10 +7,13 @@
   $api_port = '8776',
   $api_mode = 'tcp',
   $backend_port = '8776',
+  $backend_ssl_port = '8776 ssl crt /etc/ssl/horizon.pem',
   $log = 'tcplog',
 ) {
 
   include quickstack::load_balancer::common
+
+  if str2bool("$::haproxy_cert_exist") {
 
   quickstack::load_balancer::proxy { 'cinder-api':
     addr                 => [ $frontend_priv_host,
@@ -24,7 +27,7 @@
   }
   quickstack::load_balancer::proxy { 'cinder-api-pub':
     addr                 => [ $frontend_pub_host ],
-    port                 => "8776 ssl crt /etc/ssl/horizon.pem",
+    port                 => "$backend_ssl_port",
     mode                 => "$api_mode",
     listen_options       => { 'option' => [ "$log" ] },
     member_options       => [ 'check inter 1s' ],
@@ -32,4 +35,19 @@
     backend_server_addrs => $backend_server_addrs,
     backend_server_names => $backend_server_names,
   }
+ }
+ else {
+   quickstack::load_balancer::proxy { 'cinder-api':
+    addr                 => [ $frontend_pub_host,
+                              $frontend_priv_host,
+                              $frontend_admin_host ],
+    port                 => "$api_port",
+    mode                 => "$api_mode",
+    listen_options       => { 'option' => [ "$log" ] },
+    member_options       => [ 'check inter 1s' ],
+    backend_server_addrs => $backend_server_addrs,
+    backend_server_names => $backend_server_names,
+  }
+ }
+
 }
